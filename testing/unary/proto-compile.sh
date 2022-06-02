cecaa720b5d75ab808fde109f5e78c1c4a0e2bed
Nathaniel Singer <nathaniel@singer.cloud>
2022-06-02 02:07:50 -0700

    new testing

---
+++ testing/unary/proto-compile.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+python3 -m grpc_tools.protoc --proto_path=. ./unary.proto --python_out=. --grpc_python_out=.
---
+++ testing/unary/unary_client.py
@@ -0,0 +1,39 @@
+import unary_pb2_grpc as pb2_grpc
+import unary_pb2 as pb2
+import grpc
+
+class UnaryClient(object):
+    """
+    Client for sending a sample gRPC message
+    """
+
+    def __init__(self):
+        self.host = 'localhost'
+        self.port = 50051
+
+        # instantiate a channel
+        self.channel = grpc.insecure_channel(f"{self.host}:{self.port}")
+
+        # bind the client and the server
+        self.stub = pb2_grpc.UnaryStub(self.channel)
+
+    def start_honeypot(self):
+        """
+        Client function to call the rpc for StartHoneypot
+        """
+        message = pb2.StartHoneypot(
+            hpType = 1,
+            ownerId = 987654321,
+            startAfterBuild = True,
+            targetIp = 2923507574,
+            targetSubnet = 4294934528,
+            resourceAllocation = 2
+        )
+        print(f"Sending: {message}")
+        return self.stub.GetServerResponse(message)
+
+
+if __name__ == "__main__":
+    client = UnaryClient()
+    result = client.start_honeypot()
+    print(f"Received: {result}")
