e0a8424206e662a8ada3fd93d72761715e6904f2
SharpMan <dargomere@live.com>
2022-06-10 16:41:29 -0700

    Add Docker setup file

--- generate-certificate.sh
+++ generate-certificate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # REMEMBER: CN needs to be set correctly or the tunnel will fail
+echo "Creating cert directory..."
+mkdir -p cert
 echo "Generating keypair..."
 openssl req -newkey rsa:4096 -nodes -sha512 -x509 -days 3650 -nodes -out cert/server.crt -keyout cert/server.key
 echo "DONE"
